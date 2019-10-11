eb1c4541115eab53c446949f12f7a810eaa0e115
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-10-11 15:21:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // Write your code here
 class Breakfast {
-  constructor (food, drink){}
-  
+  constructor (food, drink){
+    
+    
+    this.food = food;
+    this.drink = drink;
+  }
 }
 class Lunch {
   
-constructor(salas, soup, drink){}
+constructor(salad, soup, drink){
+  
+this.salad = salad;
+this.soup = soup;
+this.drink = drink;
 }
-class Dinner {}
+}
+class Dinner {
+  constructor(salad,soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+  
+}
